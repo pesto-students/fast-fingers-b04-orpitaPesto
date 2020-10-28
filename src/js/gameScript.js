718586c1fa4624ee4f358451217ac3a854021d5c
OrpitaPesto <orpitadutta91@gmail.com>
2020-10-28 13:20:02 +0530

    added some style changes

--- src/js/gameScript.js
+++ src/js/gameScript.js
@@ -172837,11 +172837,11 @@ let TIME_LIMIT = 30;
 
 document.onreadystatechange = function() { 
     if (document.readyState !== "complete") { 
-        document.querySelector("body").style.display = "none"; 
+        document.getElementById('container-loaded').style.visibility = "hidden"; 
         document.querySelector("#loader").style.visibility = "visible"; 
     } else { 
         document.querySelector("#loader").style.display = "none"; 
-        document.querySelector("body").style.visibility = "visible"; 
+        document.getElementById('container-loaded').style.visibility = "visible"; 
     } 
 }; 
 window.onload = function() {
